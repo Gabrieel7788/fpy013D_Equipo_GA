6f89672da5771819fce3f6f016f835ccbcc5f4c8
Mauricio Jaramillo <mau.jaramillom@duocuc.cl>
2025-06-29 02:23:43 -0400

    Update #base del programa.py
    Agrega función Mauricio

--- #base del programa.py
+++ #base del programa.py
@@ -1,10 +1,13 @@
 def datos_gabriel():
     print("Mi nombre es Gabriel y tengo 20 años.")
+def datos_mauricio():
+    print("Mi nombre es Mauricio Jaramillo y tengo 29 años.")
+
 # Menú base del programa
 while True:
     print("\n--- MENÚ PRINCIPAL ---")
     print("1. Función de Gabriel")
-    print("2. Función de integrante 2")
+    print("2. Función de Mauricio")
     print("0. Salir")
     op = input("Seleccione opción: ")
     if op == "0":
@@ -13,6 +16,6 @@ while True:
     elif op == "1":
         datos_gabriel() # Aquí se llamará a la función del integrante 1
     elif op == "2":
-        pass # Aquí se llamará a la función del integrante 2
+        datos_mauricio() # Aquí se llamará a la función del integrante 2
     else:
         print(" Opción inválida.")
